7a2c9617a4045d8d5c50998a8a267cffac44de33
Michael Robinson <laxdog@gmail.com>
2017-09-06 17:33:15 +0100

    Add ETH, this is hacky and will be changed. Just a quick fix to display the extra currencies

--- server.py
+++ server.py
@@ -20,7 +20,7 @@ def run_query(query, db):
 @app.route("/crypto-graph/polo.json")
 def polo():
     db = "polo.db"
-    query = "SELECT unixtime, round(rate0 * 100, 5) FROM loans WHERE unixtime % 3 = 0 ORDER BY unixtime ASC;"
+    query = "SELECT unixtime, round(rate0 * 100, 5) FROM loans WHERE unixtime % 10 = 0 ORDER BY unixtime ASC;"
     results = run_query(query, db)
     return json_min(results)
 
@@ -29,7 +29,43 @@ def polo():
 @app.route("/crypto-graph/finex.json")
 def finex():
     db = "finex.db"
-    query = "SELECT unixtime, round(rate0 * 100, 5) FROM loans ORDER BY unixtime ASC;"
+    query = "SELECT unixtime, round(rate0 * 100, 5) FROM loans WHERE unixtime % 2 = 0 ORDER BY unixtime ASC;"
+    results = run_query(query, db)
+    return json_min(results)
+
+
+@app.route("/eth-polo.json")
+@app.route("/crypto-graph/eth-polo.json")
+def eth_polo():
+    db = "ETH-polo.db"
+    query = "SELECT unixtime, round(rate0 * 100, 5) FROM loans WHERE unixtime % 60 = 0 ORDER BY unixtime ASC;"
+    results = run_query(query, db)
+    return json_min(results)
+
+
+@app.route("/eth-finex.json")
+@app.route("/crypto-graph/eth-finex.json")
+def eth_finex():
+    db = "ETH-finex.db"
+    query = "SELECT unixtime, round(rate0 * 100, 5) FROM loans WHERE unixtime % 60 = 0 ORDER BY unixtime ASC;"
+    results = run_query(query, db)
+    return json_min(results)
+
+
+@app.route("/btc-polo.json")
+@app.route("/crypto-graph/btc-polo.json")
+def btc_polo():
+    db = "BTC-polo.db"
+    query = "SELECT unixtime, round(rate0 * 100, 5) FROM loans WHERE unixtime % 60 = 0 ORDER BY unixtime ASC;"
+    results = run_query(query, db)
+    return json_min(results)
+
+
+@app.route("/btc-finex.json")
+@app.route("/crypto-graph/btc-finex.json")
+def btc_finex():
+    db = "BTC-finex.db"
+    query = "SELECT unixtime, round(rate0 * 100, 5) FROM loans WHERE unixtime % 60 = 0 ORDER BY unixtime ASC;"
     results = run_query(query, db)
     return json_min(results)
 
